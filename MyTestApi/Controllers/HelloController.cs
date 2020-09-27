f98c7eb12e343303f2a549d9980e4b1b73c1141b
Yongson Kim <yongson.kim@vntgcorp.com>
2020-09-27 21:33:10 +0900

    테스트 api 솔루션/프로젝트 추가

---
+++ MyTestApi/Controllers/HelloController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using System.Net;
+using System.Net.Http;
+using System.ComponentModel;
+using System.Reflection;
+using System.Net.Mime;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.Extensions.Configuration;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyTestApi.Controllers
+{
+    /// <summary>
+    /// 예제
+    /// </summary>
+    [ApiController, Route("[controller]"), Produces(MediaTypeNames.Application.Json)]
+    public class HelloController : ControllerBase
+    {
+        /// <summary>
+        /// 예제
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "MES MVP", "대형압연 작업지시 서비스" };
+        }
+    }
+}
